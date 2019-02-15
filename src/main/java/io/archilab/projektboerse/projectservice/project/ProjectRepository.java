6e200f0cec2edfeacadb8606fe463ef49b85322f
Mansoor Rahmati <mansoor_rahmati@web.de>
2019-02-15 11:50:44 +0100

    Add filter by status method

--- src/main/java/io/archilab/projektboerse/projectservice/project/ProjectRepository.java
+++ src/main/java/io/archilab/projektboerse/projectservice/project/ProjectRepository.java
@@ -1,8 +1,10 @@
 package io.archilab.projektboerse.projectservice.project;
 
+import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ProjectRepository extends PagingAndSortingRepository<Project, UUID> {
 
+    Set<Project> findByStatus(ProjectStatus status);
 }
