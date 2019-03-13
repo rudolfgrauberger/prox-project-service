513cbd33bba65221d8135a10f346ea3c8631486b
JannIntveen <39293485+JannIntveen@users.noreply.github.com>
2019-03-13 14:56:37 +0100

    Removed Development @CrossOrigin

--- src/main/java/io/archilab/projektboerse/projectservice/project/ProjectRepository.java
+++ src/main/java/io/archilab/projektboerse/projectservice/project/ProjectRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin // TODO nur development
 public interface ProjectRepository extends PagingAndSortingRepository<Project, UUID> {
 
   Set<Project> findByStatus(ProjectStatus status);
