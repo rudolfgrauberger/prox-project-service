697d873e6a4357adfc606605cae890e3564aa5f0
JannIntveen <JannIntveen@gmail.com>
2019-03-13 16:14:20 +0100

    ModuleRepository is now mapped to path projectModules

--- src/main/java/io/archilab/projektboerse/projectservice/module/ModuleRepository.java
+++ src/main/java/io/archilab/projektboerse/projectservice/module/ModuleRepository.java
@@ -1,10 +1,14 @@
 package io.archilab.projektboerse.projectservice.module;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@RepositoryRestResource(collectionResourceRel = "projectModules", path = "projectModules")
 public interface ModuleRepository extends PagingAndSortingRepository<Module, UUID> {
+
     Optional<Module> findByExternalModuleID(ExternalModuleID externalModuleID);
 }
