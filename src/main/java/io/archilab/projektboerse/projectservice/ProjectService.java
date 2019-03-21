d46893e2318cbc5d4a1fd527f675d0e5c2e6d9be
Stephan <44501469+stephanhhhh@users.noreply.github.com>
2019-03-21 16:48:25 +0100

    test

--- src/main/java/io/archilab/projektboerse/projectservice/ProjectService.java
+++ src/main/java/io/archilab/projektboerse/projectservice/ProjectService.java
@@ -6,15 +6,48 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.*; 
+import java.net.*; 
+  
+
+
 @SpringBootApplication
 @EnableFeignClients
 public class ProjectService {
 
   public static void main(String[] args) {
+	  
+	  
+	  
+	  
     ConfigurableApplicationContext context = SpringApplication.run(ProjectService.class, args);
 
     context.getBean(ModuleService.class).importModules();
+    
+    
+    String ipAddress = "login.coalbase.io"; 
+    try {
+		sendPingRequest(ipAddress);
+	} catch (UnknownHostException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} 
   }
 
+  
+  public static void sendPingRequest(String ipAddress) 
+          throws UnknownHostException, IOException 
+{ 
+InetAddress geek = InetAddress.getByName(ipAddress); 
+System.out.println("Sending Ping Request to " + ipAddress); 
+if (geek.isReachable(5000)) 
+  System.out.println("Host is reachable"); 
+else
+  System.out.println("Sorry ! We can't reach to this host"); 
+} 
+  
 }
 
